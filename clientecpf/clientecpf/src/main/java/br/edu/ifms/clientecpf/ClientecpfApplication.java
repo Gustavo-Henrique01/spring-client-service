7ee7600a72c0f3129a43617b804576e4648d3b65
Gustavo-Henrique01 <gusta.2019.henrique@gmail.com>
2024-12-07 16:32:24 -0400

    criação do projeto spring cliente, cpf e clientecpf

---
+++ clientecpf/clientecpf/src/main/java/br/edu/ifms/clientecpf/ClientecpfApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.clientecpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientecpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientecpfApplication.class, args);
+	}
+
+}
