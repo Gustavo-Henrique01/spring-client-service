843340a59f4ce5ad9422b19b90f89d22be029481
Gustavo-Henrique01 <gusta.2019.henrique@gmail.com>
2024-12-08 12:24:17 -0400

    criação da class cliente e cliente controller, junto com o methodo para buscar um cliente via requisição GET, o teste via POSTMAN foi bem sucedido

---
+++ cliente/cliente/src/main/java/br/edu/ifms/cliente/controller/ClienteController.java
@@ -0,0 +1,26 @@
+package br.edu.ifms.cliente.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifms.cliente.modelo.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Cliente>buscarCliente(@PathVariable Long codigo) throws Exception {
+
+	Cliente cliente = new Cliente(codigo, "cliente1", "000.000.000-01");
+		
+		Thread.sleep(3000);
+
+		return ResponseEntity.ok(cliente);
+	}
+	
+}
+	
